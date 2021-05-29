dc4ad079bae8b703fbf9f61cd421a4d67eb14122
Arvind Rao <47332035+arvind73@users.noreply.github.com>
2021-05-29 18:44:05 +0530

    Create Interview Question 1.py

---
+++ Interview Question 1.py
@@ -0,0 +1,12 @@
+Write a function to check for any common item in the given strings. Return true if anything is common, else return false.
+
+def func(x,y):
+    for i in range(len(x)):
+        for j in range(len(y)):
+            if(x[i]==y[j]):
+                return True
+        return False
+
+
+print(func(['a','b','c'],['x','y','z','a']))  # return true
+print(func(['a','b','c'],['x','y','z']))  # return false
