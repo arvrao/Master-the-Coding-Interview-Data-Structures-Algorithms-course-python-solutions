44a0b0e7fc8b4c7ba662624762e18385e5503fea
Arvind Rao <47332035+arvind73@users.noreply.github.com>
2021-07-03 15:01:34 +0530

    Update Interview Question 1.py

--- Interview Question 1.py
+++ Interview Question 1.py
@@ -1,4 +1,4 @@
-Write a function to check for any common item in the given strings. Return true if anything is common, else return false.
+Write a function to check for any common item in the given list of strings. Return true if anything is common, else return false.
 
 def func(x,y):
     for i in range(len(x)):
